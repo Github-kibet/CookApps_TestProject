56e1394ef9294d036cee5dd269481eef81f5b86e
Github-kibet <70249708+Github-kibet@users.noreply.github.com>
2023-08-01 14:27:01 +0900

    FSM 기초 설계 및 플레이어 기본 상태 추가

---
+++ Assets/Scripts/Player/PlayerProfile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class PlayerProfile : ScriptableObject
+    {
+        
+    }
+}
